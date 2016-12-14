95d5f37adf922724dc65b5fb496bb847be101cdb
ldodson911 <ldodson911@aol.com>
2016-12-14 16:22:51 -0600

    added edits

--- index.php
+++ index.php
@@ -28,6 +28,12 @@ get_header(); ?>
 
 	<?php get_sidebar(); ?>
 </div>
+<?php if ( have_posts() ): ?>
+	<div id="navigation" class="container">
+        <div class="left"><?php next_posts_link('&larr; <span>Older Posts</span>'); ?></div>
+        <div class="right"><?php previous_posts_link('<span>Newer Posts</span> &rarr;'); ?></div>
+    </div>
+<?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
