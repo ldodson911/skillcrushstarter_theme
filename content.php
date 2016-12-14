1d3bb18c3be435cd6e0f742d1332716c7a083f41
ldodson911 <ldodson911@aol.com>
2016-12-13 22:12:19 -0600

    changed placement of author tag

--- content.php
+++ content.php
@@ -1,8 +1,9 @@
 <article id="post-<?php the_ID(); ?>" class="post-entry">
 	<div class="entry-wrap">
+		<p><?php the_date(); ?></p>
 		<header class="entry-header">
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			<p><?php the_date(); ?>, <?php the_author(); ?></p>
+			
 		</header>
 		<div class="entry-summary">
 			<?php the_content(); ?>
