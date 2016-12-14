06e37f206e56649ec19d0be49e14eb5d5c32c903
ldodson911 <ldodson911@aol.com>
2016-12-13 22:05:01 -0600

    added date and author

--- content.php
+++ content.php
@@ -2,6 +2,7 @@
 	<div class="entry-wrap">
 		<header class="entry-header">
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p><?php the_date(); ?>, <?php the_author(); ?></p>
 		</header>
 		<div class="entry-summary">
 			<?php the_content(); ?>
